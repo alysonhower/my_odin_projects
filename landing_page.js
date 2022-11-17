bc19e4eacbbd6a3fbea9d2a923024b1e1524100f
Alysonhower <a@194820.xyz>
2022-11-17 13:20:35 -0300

    Apply Airbnb Javascript style with Eslint

--- landing_page.js
+++ landing_page.js
@@ -1,58 +1,55 @@
-let now_playing = document.querySelector(".now-playing");
-let track_art = document.querySelector(".track-art");
-let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
+const now_playing = document.querySelector('.now-playing');
+const track_art = document.querySelector('.track-art');
+const track_name = document.querySelector('.track-name');
+const track_artist = document.querySelector('.track-artist');
 
-let playpause_btn = document.querySelector(".playpause-track");
-let next_btn = document.querySelector(".next-track");
-let prev_btn = document.querySelector(".prev-track");
+const playpause_btn = document.querySelector('.playpause-track');
+const next_btn = document.querySelector('.next-track');
+const prev_btn = document.querySelector('.prev-track');
 
-let seek_slider = document.querySelector(".seek_slider");
-let volume_slider = document.querySelector(".volume_slider");
-let curr_time = document.querySelector(".current-time");
-let total_duration = document.querySelector(".total-duration");
+const seek_slider = document.querySelector('.seek_slider');
+const volume_slider = document.querySelector('.volume_slider');
+const curr_time = document.querySelector('.current-time');
+const total_duration = document.querySelector('.total-duration');
 
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
 // Create new audio element
-let curr_track = document.createElement('audio');
+const curr_track = document.createElement('audio');
 
 // Define the tracks that have to be played
-let track_list = [
+const track_list = [
   {
-    name: "Space Echo",
-    artist: "A.L.I.S.O.N",
-    image: "./landing_page/drive.gif",
-    path: "./landing_page/space_echo.mp3"
+    name: 'Space Echo',
+    artist: 'A.L.I.S.O.N',
+    image: './landing_page/drive.gif',
+    path: './landing_page/space_echo.mp3',
     // image source: https://www.reddit.com/r/outrun/comments/9vo6rj/drive/
   },
   {
-    name: "Passager",
-    artist: "A.L.I.S.O.N",
-    image: "./landing_page/space_render.gif",
-    path: "./landing_page/passenger.mp3"
+    name: 'Passager',
+    artist: 'A.L.I.S.O.N',
+    image: './landing_page/space_render.gif',
+    path: './landing_page/passenger.mp3',
     // image source: https://giphy.com/gifs/cyberpunk-synthwave-retrowave-XZbzPwR07W0DVYmBVx
   },
   {
-    name: "Golden Dust",
-    artist: "A.L.I.S.O.N",
-    image: "./landing_page/rainbow_glow.gif",
-    path: "./landing_page/golden_dust.mp3"
+    name: 'Golden Dust',
+    artist: 'A.L.I.S.O.N',
+    image: './landing_page/rainbow_glow.gif',
+    path: './landing_page/golden_dust.mp3',
   },
 ];
 
 function random_bg_color() {
-
   // Get a number between 64 to 256 (for getting lighter colors)
   // let red = Math.floor(Math.random() * 256) + 64;
   // let green = Math.floor(Math.random() * 256) + 64;
   // let blue = Math.floor(Math.random() * 256) + 64;
-
   // Construct a color withe the given values
   // let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
-
   // Set the background to that color
   // document.body.style.background = bgColor;
 }
@@ -63,19 +60,19 @@ function loadTrack(track_index) {
   curr_track.src = track_list[track_index].path;
   curr_track.load();
 
-  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_art.style.backgroundImage = `url(${track_list[track_index].image})`;
   track_name.textContent = track_list[track_index].name;
   track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+  now_playing.textContent = `PLAYING ${track_index + 1} OF ${track_list.length}`;
 
   updateTimer = setInterval(seekUpdate, 1000);
-  curr_track.addEventListener("ended", nextTrack);
+  curr_track.addEventListener('ended', nextTrack);
   random_bg_color();
 }
 
 function resetValues() {
-  curr_time.textContent = "00:00";
-  total_duration.textContent = "00:00";
+  curr_time.textContent = '00:00';
+  total_duration.textContent = '00:00';
   seek_slider.value = 0;
 }
 
@@ -96,27 +93,25 @@ function playTrack() {
 function pauseTrack() {
   curr_track.pause();
   isPlaying = false;
-  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
 
 function nextTrack() {
-  if (track_index < track_list.length - 1)
-    track_index += 1;
+  if (track_index < track_list.length - 1) track_index += 1;
   else track_index = 0;
   loadTrack(track_index);
   playTrack();
 }
 
 function prevTrack() {
-  if (track_index > 0)
-    track_index -= 1;
+  if (track_index > 0) track_index -= 1;
   else track_index = track_list.length;
   loadTrack(track_index);
   playTrack();
 }
 
 function seekTo() {
-  let seekto = curr_track.duration * (seek_slider.value / 100);
+  const seekto = curr_track.duration * (seek_slider.value / 100);
   curr_track.currentTime = seekto;
 }
 
@@ -133,18 +128,28 @@ function seekUpdate() {
     seek_slider.value = seekPosition;
 
     let currentMinutes = Math.floor(curr_track.currentTime / 60);
-    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let currentSeconds = Math.floor(
+      curr_track.currentTime - currentMinutes * 60,
+    );
     let durationMinutes = Math.floor(curr_track.duration / 60);
-    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+    let durationSeconds = Math.floor(
+      curr_track.duration - durationMinutes * 60,
+    );
 
-    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = `0${currentMinutes}`;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = `0${durationMinutes}`;
+    }
 
-    curr_time.textContent = currentMinutes + ":" + currentSeconds;
-    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    curr_time.textContent = `${currentMinutes}:${currentSeconds}`;
+    total_duration.textContent = `${durationMinutes}:${durationSeconds}`;
   }
 }
-
-
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,34 +1,33 @@
 function game() {
   function getPlayerSelection() {
-    const playerSelection = prompt('Choose your weapon')
+    const playerSelection = prompt('Choose your weapon');
     return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
   }
 
   function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice()
-    playerSelection = getPlayerSelection()
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerSelection();
     if (computerSelection === playerSelection) {
-      console.log(`You chose ${playerSelection}. Computer also chose ${computerSelection}. Tie game!`)
+      console.log(`You chose ${playerSelection}. Computer also chose ${computerSelection}. Tie game!`);
     } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
-      console.log('Rock beats Scissors! You win!')
+      console.log('Rock beats Scissors! You win!');
     } else if (computerSelection === 'Rock' && playerSelection === 'Paper') {
-      console.log('Paper beats Rock! You win!')
+      console.log('Paper beats Rock! You win!');
     } else if (computerSelection === 'Paper' && playerSelection === 'Scissors') {
-      console.log('Scissors beats Paper! You win!')
+      console.log('Scissors beats Paper! You win!');
     } else {
-      console.log(`Computer chose ${computerSelection}. You chose ${playerSelection}. You loose!`)
+      console.log(`Computer chose ${computerSelection}. You chose ${playerSelection}. You loose!`);
     }
   }
-
 
   for (let i = 0; i < 5; i++) {
     function getComputerChoice() {
-      const choises = ['Rock', 'Paper', 'Scissors']
-      const choise = Math.floor(Math.random() * choises.length)
-      return (choises[choise])
+      const choises = ['Rock', 'Paper', 'Scissors'];
+      const choise = Math.floor(Math.random() * choises.length);
+      return (choises[choise]);
     }
-    playRound()
+    playRound();
   }
 }
 
-game()
+game();
